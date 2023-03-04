a1a646717525a5a4ed2ce88e1a0312c032ac57bd
yiwan08 <69824146+yiwan08@users.noreply.github.com>
2023-03-04 13:24:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,89 @@
-    // the link to your model provided by Teachable Machine export panel
-    const URL = "https://teachablemachine.withgoogle.com/models/mkVQ5QU5d/";
+// the link to your model provided by Teachable Machine export panel
+const URL = "https://teachablemachine.withgoogle.com/models/mkVQ5QU5d/";
 
-    let model, webcam, labelContainer, maxPredictions;
+let model, webcam, labelContainer, maxPredictions;
 
-    const resultContainer = document.getElementById("result");
-    const startButton = document.getElementById("start-button")
+const resultContainer = document.getElementById("result");
+const startButton = document.getElementById("start-button")
 
-    const classLabels = ["Cirrus", "Cirrocumulus", "Cirrostratus", "Altocumulus","Altostratus", "Nimbostratus", "Cumulonimbus", "Cumulus", "Stratus", "Stratocumulus"];
+const classLabels = ["Cirrus", "Cirrocumulus", "Cirrostratus", "Altocumulus","Altostratus", "Nimbostratus", "Cumulonimbus", "Cumulus", "Stratus", "Stratocumulus"];
 
-    // Load the image model and setup the webcam
-    async function init() {
-        const modelURL = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
 
-        // load the model and metadata
-        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-        // or files from your local hard drive
-        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-        model = await tmImage.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
 
-        // Convenience function to setup a webcam
-        const flip = true; // whether to flip the webcam
-        webcam = new tmImage.Webcam(600, 600, flip); // width, height, flip
-        await webcam.setup(); // request access to the webcam
-        await webcam.play();
-        window.requestAnimationFrame(loop);
+    // Convenience function to setup a webcam
+    const flip = true; // whether to flip the webcam
+    webcam = new tmImage.Webcam(800, 500, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
 
-        // append elements to the DOM
-        document.getElementById("webcam-container").appendChild(webcam.canvas);
-        labelContainer = document.getElementById("label-container");
-        for (let i = 0; i < maxPredictions; i++) { // and class labels
-            labelContainer.appendChild(document.createElement("div"));
-        }
+    // append elements to the DOM
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        labelContainer.appendChild(document.createElement("div"));
     }
+}
 
-    async function loop() {
-        webcam.update(); // update the webcam frame
-        await predict();
-        window.requestAnimationFrame(loop);
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
+var scores = [];
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+  
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction = prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+      var currentNode = labelContainer.childNodes[i];
+        currentNode.innerHTML = classPrediction;
+        scores.push(prediction[i].probability);
+      
+    if (prediction[i].probability.toFixed(2) >= 0.70) {
+      currentNode.style.background = "light green";
     }
+  }
 
-    // run the webcam image through the image model
-    var scores = [];
-    async function predict() {
-        // predict can take in an image, video or canvas html element
-        const prediction = await model.predict(webcam.canvas);
+var maxScore = Math.max(...scores);
+var maxLabel = classLabels[scores.indexOf(maxScore)]
+result.innerHTML = maxLabel;
 
-        for (let i = 0; i < maxPredictions; i++) {
-            const classPrediction = prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-            labelContainer.childNodes[i].innerHTML = classPrediction;
-            scores.push(prediction[i].probability);
-        }
-
-        for (let i = 0; i < scores.length; i++) {
-            var maxScore = scores[0];
-            var maxLabelIndex = 0;
-            if (scores[i] > maxScore) {
-                maxScore = scores;
-                maxLabelIndex = i;
-            }
-            result.innerHTML = classLabels[maxLabelIndex];
-        }
-        scores = [];
-    }
+  if (maxLabel === "Cirrus") {
+    weather.innerHTML = "Indicates warm air will form; generally good weather; no precipitation!";
+  } else if (maxLabel === "Cirrocumulus") {
+    weather.innerHTML = "Fair weather; slight chance for storm.";
+  } else if (maxLabel === "Cirrostratus") {
+    weather.innerHTML = "Either persistent rain within a day or a light drizzle!";
+  } else if (maxLabel === "Altocumulus") {
+    weather.innerHTML = "Nothing will happen soon, enjoy the sun!";
+  } else if (maxLabel === "Altostratus") {
+    weather.innerHTML = "Usually indicates rain or snow to come but generally light precipitation~";
+  } else if (maxLabel === "Nimbostratus") {
+    weather.innerHTML = "Leads to continuous moderate precipitation that lasts for several hours.";
+  } else if (maxLabel === "Cumulonimbus") {
+    weather.innerHTML = "Leads to extreme weather such as heavy downpours and tornadoes. Be careful!";
+  } else if (maxLabel === "Cumulus") {
+    weather.innerHTML = "It's a sunny day! But may develops into cumulonimbus which causes precipitation...";
+  } else if (maxLabel === "Stratus") {
+    weather.innerHTML = "Usually no rainfall, sometimes slight drizzle, light snow if cold enough.";
+  } else if (maxLabel === "Stratocumulus") {
+    weather.innerHTML = "Most common type of cloud; does not mean anything :]";
+  } else {
+    weather.innerHTML = "N/A";
+  }
+  scores = [];
+}
